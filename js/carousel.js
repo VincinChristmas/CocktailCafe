5f5ded5fca6b995110c85905efe4d042e5610e01
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-03 15:51:55 +0800

    I fixed the proplem and added a new feature

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,24 @@
-document.querySelector('.theCocktailButtonRotation').addEventListener('click', setInterval(getDrink, 6000)) 
+let intervalID = null; 
+
+document.querySelector('.theCocktailButtonRotation').addEventListener('click', function() {
+    if (intervalID) {
+        clearInterval(intervalID); 
+        intervalID = null; 
+    } else {
+        intervalID = setInterval(getDrink, 6000); 
+    }
+});
 
 function getDrink() {
-fetch('https:www.thecocktaildb.com/api/json/v1/1/random.php')
-.then(res => res.json())
-.then(data => {
-    console.log(data.drinks[0])
-    document.querySelector('h2').innerText = data.drinks[0].strDrink
-    document.querySelector('img').src = data.drinks[0].strDrinkThumb
-    document.querySelector('h3').innerText = data.drinks[0].strInstructions
-})
-.catch(err => {
-    console.log('error $(err)')
-})
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.drinks[0]);
+        document.querySelector('h2').innerText = data.drinks[0].strDrink;
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb;
+        document.querySelector('h3').innerText = data.drinks[0].strInstructions;
+    })
+    .catch(err => {
+        console.log('error', err);
+    });
 }

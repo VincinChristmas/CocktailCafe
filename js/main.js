9b7ab12a17995e297e2ba6e9fcc5306f8874cd5a
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-04 03:41:58 +0800

    made more drink options become available

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function getDrink() {
 fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
 .then(res => res.json()) 
 .then(data => {
-    console.log(data.drinks[0])
+    console.log(data.drinks)
     document.querySelector('h2').innerText = data.drinks[0].strDrink
     document.querySelector('img').src = data.drinks[0].strDrinkThumb
     document.querySelector('h3').innerText = data.drinks[0].strInstructions

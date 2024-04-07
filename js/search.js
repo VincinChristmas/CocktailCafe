9367e54827c67bcb70d46c72ff7e49c408a23a29
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-07 10:46:01 +0800

    fixed a link

--- js/search.js
+++ js/search.js
@@ -1,11 +1,12 @@
 document.querySelector('.getCocktail').addEventListener('click', getDrink) 
+console.log("button clicked")
 
 
 
 function getDrink() {
     let drink = document.querySelector('input').value
 
-fetch(`https:www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
 .then(res => res.json())
 .then(data => {
     console.log(data.drinks)
@@ -14,6 +15,6 @@ fetch(`https:www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     document.querySelector('h3').innerText = data.drinks[0].strInstructions
 })
 .catch(err => {
-    console.log('error $(err)')
-})
+    console.log('error', err);
+});
 }
